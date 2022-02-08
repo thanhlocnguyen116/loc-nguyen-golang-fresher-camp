908035d3099d72684f73ebbacdcb258fa44e88b6
Loc Nguyen <thanhlocnguyen116@gmail.com.vn>
2022-02-08 14:53:36 +0700

    GoEx10

--- common/sql_model.go
+++
@@ -1,9 +0,0 @@
-package common
-
-import "time"
-
-type SQLModel struct {
-	Status    int        `json:"status" gorm:"status"`
-	CreatedAt *time.Time `json:"created_at" gorm:"created_at"`
-	UpdateAt  *time.Time `json:"updated_at" gorm:"updated_at"`
-}
--- modules/users/usermodel/filter.go
+++
@@ -1,5 +0,0 @@
-package usermodel
-
-type Filter struct {
-	User_id int `json:"user_id,omitempty" form:"user_id"`
-}
